0081a6930c707dfed6390fb8fd4a9aa716bac762
kityprincess <stowekm@byui.edu>
2018-05-07 18:03:46 -0700

    TOA Week 3.1

---
+++ web/stowe_toa_3.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Stowe TOA Week 03 PHP</title>
+</head>
+<body>
+	Name: <?php echo $_POST["name"]; ?><br>
+	Email: <?php echo $_POST["email"]; ?><br>
+	Major: <?php echo $_POST["major"]; ?><br>
+	Comments: <?php echo $_POST["comments"]; ?><br>
+</body>
+</html>
