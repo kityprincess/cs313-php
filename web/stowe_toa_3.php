94900309e94422617c9389eceee4aa4907057897
kityprincess <stowekm@byui.edu>
2018-05-08 19:00:16 -0700

    TOA Week 3.7

--- web/stowe_toa_3.php
+++ web/stowe_toa_3.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 	Name: <?php echo $_POST["name"]; ?><br>
-	Email: <a href="<?php echo "mailto:".$_POST["email"]; ?>"><?php echo $_POST["email"]; ?><br> 
+	Email: <a href="<?php echo "mailto:".$_POST["email"]; ?>"><?php echo $_POST["email"]; ?></a><br> 
 	Major: <?php echo $_POST["major"]; ?><br>
 	Comments: <?php echo $_POST["comments"]; ?><br>
 </body>
