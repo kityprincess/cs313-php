ee620ed725b23334385647fc9d0187182914d967
kityprincess <stowekm@byui.edu>
2018-05-08 18:52:35 -0700

    TOA Week 3.5

--- web/stowe_toa_3.php
+++ web/stowe_toa_3.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 	Name: <?php echo $_POST["name"]; ?><br>
-	Email: <?php echo "mailto:$_POST["email"]"; ?><br> 
+	Email: <?php echo "mailto:$_POST['email']"; ?><br> 
 	/* need javascript to convert addy to mailto:? https://www.webdeveloper.com/forum/d/185032-how-to-convert-text-input-into-hyperlink */
 	Major: <?php echo $_POST["major"]; ?><br>
 	Comments: <?php echo $_POST["comments"]; ?><br>
