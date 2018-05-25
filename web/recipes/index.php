822135ea9aad88bdc3da81b89aef159ad583ee43
kityprincess <stowekm@byui.edu>
2018-05-25 16:49:10 -0700

    Stowe Ponder 5.4

--- web/recipes/index.php
+++ web/recipes/index.php
@@ -7,7 +7,21 @@
 * Description: Queries PostgreSQL database from PHP.
 ***********************************************************/
 require "dbConnect.php";
-$db = get_db();
+$dbUrl = getenv('DATABASE_URL');
+
+$dbopts = parse_url($dbUrl);
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$rows = null;
 ?>
 <!DOCTYPE html>
 <html>
