396eaba6efca7805119e28a3c70e0d92709f738f
kityprincess <stowekm@byui.edu>
2018-05-25 19:06:59 -0700

    Stowe Ponder 5.18

--- web/recipes/connectDB.php
+++ web/recipes/connectDB.php
@@ -1,31 +1,14 @@
 <?php
-/**********************************************************
-* File: dbConnect.php
-* Author: Br. Burton
-* 
-* Description: Shows how to connect using either local
-* OR Heroku credentials, depending on whether the code
-* is executing at heroku.
-***********************************************************/
-function get_db() {
-	$db = NULL;
+$dbUrl = getenv('DATABASE_URL');
 
-	echo "Testing";
+$dbopts = parse_url($dbUrl);
 
-		// default Heroku Postgres configuration URL
-		$dbUrl = getenv('DATABASE_URL');
-		
-		// Get the various parts of the DB Connection from the URL
-		$dbopts = parse_url($dbUrl);
-		$dbHost = $dbopts["host"];
-		$dbPort = $dbopts["port"];
-		$dbUser = $dbopts["user"];
-		$dbPassword = $dbopts["pass"];
-		$dbName = ltrim($dbopts["path"],'/');
-		// Create the PDO connection
-		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
-		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
 
-	return $db;
-}
+$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
