076a4efa02ad9f593ff88602e41e4e8c3ebee555
kityprincess <stowekm@byui.edu>
2018-05-26 08:43:31 -0700

    Ponder 5.39

--- web/recipes/index.php
+++ web/recipes/index.php
@@ -41,22 +41,14 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     <br/>
     <br/>
 </p>
+
+        <form action="index.php" class="search" method="post">
+            <strong><label for="name">Name:</label></strong>
+            <input type="text" name="name" id="name">
+            <input type="submit" value="Search">
+        </form>
 <?php
-// In this example, for simplicity, the query is executed
-// right here and the data echoed out as we iterate the query.
-// You could imagine that in a more involved application, we
-// would likely query the database in a completely separate file / function
-// and build a list of objects that held the components of each
-// scripture. Then, here on the page, we could simply call that 
-// function, and iterate through the list that was returned and
-// print each component.
-// First, prepare the statement
-// Notice that we avoid using "SELECT *" here. This is considered
-// good practice so we don't inadvertently bring back data we don't
-// want, especially if the database changes later.
-//$statement = $db->prepare("SELECT name FROM recipe");
-//$statement->execute();
-// Go through each result
+
 $rows = null;
 
 if(!empty($_POST['name'])) {
@@ -82,13 +74,5 @@ foreach($rows as $row) {
     echo '</p>';
 }
 ?>
-
-        <br>
-        <form action="index.php" class="search" method="post">
-            <strong><label for="name">Name:</label></strong>
-            <input type="text" name="name" id="name">
-            <input type="submit" value="Search">
-        </form>
-
     </body>
 </html>
--- web/recipes/recipeDetails.php
+++ web/recipes/recipeDetails.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="recipes.css"/>
-    <title>Kimberly's CS313 World</title>
+    <title>Heirloom Recipes</title>
   </head>
   <body>
 <?php
