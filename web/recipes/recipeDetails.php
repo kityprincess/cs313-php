917d4bd1aed36c1b42e517e4c37eebb1cadaef8b
kityprincess <stowekm@byui.edu>
2018-05-26 22:15:43 -0700

    Ponder 5.49

--- web/recipes/recipeDetails.php
+++ web/recipes/recipeDetails.php
@@ -25,25 +25,126 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 if(!empty($_GET['id'])){
     $id = $_GET['id'];
 
-    $stmt = $db->prepare('SELECT * FROM public.recipe JOIN recipe_ingredients ON (recipe.id = recipe_ingredients.recipe_id) WHERE recipe.id = :id');
-    $stmt->bindValue('id', $id, PDO::PARAM_INT);
-    $stmt->execute();
-    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    echo '<h1>Recipe Details</h1>';
-
-    foreach($rows as $row) {
-        var_dump($row);
-        echo '<p>';
-        echo '<strong>' . $row['name'] . '</strong>';
-        echo '</p>';
-        echo '<p>';
-        // need to echo through all ingredients
-        echo $row['ingredients.name'];
-        echo '<br/>';
-        echo $row['instructions'];
-        echo '</p>';
+    function doOneQuery($db, $query, $id)
+    {
+        $stmt = $db->prepare($query);
+        if ($stmt)
+        {
+            $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+            if ($stmt->execute())
+            {
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+        }
+        return array();
     }
+
+    function getRecipeInfo($db, $id)
+    {
+        $recipeInfo['recipe']   = doOneQuery($db,
+        'SELECT
+         r.id
+        ,r.name
+        ,r.instructions
+        FROM
+         recipe r
+        WHERE
+         r.id = :id', $id);
+
+        $recipeInfo['ingredient'] = doOneQuery($db,
+        'SELECT
+         r.id
+        ,i.name
+        ,ri.qty
+        ,u.description
+        ,u.abbr
+        FROM
+         recipe r
+          INNER JOIN
+         recipe_ingredients ri ON r.id = ri.recipe_id
+          INNER JOIN
+         ingredients i ON i.id = ri.ingredients_id
+          INNER JOIN
+         units u ON u.id = ri.unit_id
+        WHERE
+         r.id = :id', $id);
+
+        $recipeInfo['category'] = doOneQuery($db,
+        'SELECT
+         r.id
+        ,c.description
+        FROM
+         recipe r
+          INNER JOIN
+         recipe_category rc ON rc.recipe_id = r.id
+          INNER JOIN
+         category c ON c.id = rc.category_id
+        WHERE
+         r.id = :id', $id);
+        
+        $recipeInfo['media']    = doOneQuery($db,
+        'SELECT
+         r.id
+        ,m.description
+        ,m.file
+        FROM
+         recipe r
+          INNER JOIN
+         media m ON m.recipe_id = r.id
+        WHERE
+         r.id = :id', $id);
+
+        return $recipeInfo;
+    }
+
+    $rDetails = getRecipeInfo($db, $id);
+
+    echo '<pre>';
+    print_r ($rDetails);
+    echo '</pre>';
+
+    //echo '<h1>';
+    // echo $rDetails['r.name'];
+    // echo '</h1>';
+
+    // foreach ($rDetails[ingredient] AS $ing) 
+    // {
+    //     echo '<p>';
+    //     echo $ing['ri.qty'];
+    //     echo $ing['u.abbr'];
+    //     echo $ing['i.name'];
+
+    // }
+
+    // foreach($rDetails[category] AS $cat)
+    // {
+    //     echo '<p>';
+    //     echo $cat['c.description'];
+    //     echo '<br/>';
+    // }
+
+    //     foreach($rDetails[media] AS $med)
+    // {
+    //     echo '<p>';
+    //     echo $med['m.file'];
+    //     echo '<br/>';
+    // }
+
+
+
+    // echo '<h1>Recipe Details</h1>';
+
+    // foreach($rows as $row) {
+    //     echo '<p>';
+    //     echo '<strong>' . $row['name'] . '</strong>';
+    //     echo '</p>';
+    //     echo '<p>';
+    //     // need to echo through all ingredients
+    //     echo $row['ingredients_id'];
+    //     echo '<br/>';
+    //     echo $row['instructions'];
+    //     echo '</p>';
+    // }
 }
 
 else {
