bcef3f6dc21f7ade4637ead800a4dc94c045bc20
kityprincess <stowekm@byui.edu>
2018-05-31 15:59:04 -0700

    W6 - Troubleshooting Details.1

--- web/recipes/index.php
+++ web/recipes/index.php
@@ -71,7 +71,7 @@ else {
 foreach($rows as $row) {
     echo '<p>';
     echo '<a href="recipeDetails.php?id=' . $row['id'] . '">' . $row['name'] . '</a>';
-    echo '$row['id']';
+    echo $row['id'];
     echo '</p>';
 }
 ?>
