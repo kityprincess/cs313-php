9d8f0159c8e45a1b84623c52a1786d7bad26bbfa
kityprincess <stowekm@byui.edu>
2018-06-02 14:05:46 -0700

    full Db testing 2

--- web/recipes/addRecipe.php
+++ web/recipes/addRecipe.php
@@ -71,12 +71,16 @@ if (isset($_POST['qty']) && isset($_POST['unit']) && isset($_POST['ingredient'])
 
 //get ingredients ID 
   $result = $stmt->fetch();
+  echo '<pre>';
+  var_dump($result);
+  echo '</pre>';
   $ingredient_id = $result['id'];
 }
 
   echo '<pre>';
   var_dump($_POST);
   echo '</pre>';
+
 //insert data into recipe_ingredients table 
   $stmt = $db->prepare('INSERT INTO recipe_ingredients (recipe_id, ingredients_id) VALUES (:recipe_id, :ingredients_id);');
   
