983516f268e4f86cbd9bea942c60092f7fd0839a
kityprincess <stowekm@byui.edu>
2018-06-09 10:59:16 -0700

    Add update to receiptDetails.php

--- web/recipes/recipeDetails.php
+++ web/recipes/recipeDetails.php
@@ -118,6 +118,8 @@ if(!empty($_GET['id'])){
     echo '<br/>';
     echo '</p>';
 
+    echo '<a href="updateRecipe.php?id=' . $rDetails['recipe'][0]['id'] . '">'Update Recipe'</a>';
+
     echo '<a href="index.php">Go back home!</a>';
 
     echo '</p>';
