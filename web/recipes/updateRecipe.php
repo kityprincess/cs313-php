6919a5a904854642246fc10ced6c265a158bf5af
kityprincess <stowekm@byui.edu>
2018-06-09 13:18:54 -0700

    updateRecipe updateName id 4

--- web/recipes/updateRecipe.php
+++ web/recipes/updateRecipe.php
@@ -156,7 +156,7 @@ if (isset($_POST['qty']) && isset($_POST['unit']) && isset($_POST['ingredient'])
             <input type="text" name="name" id="name">
             <label for="category">Category:</label>
             <input type="text" name="category" id="category">
-            <input type="text" name="id" id="id" value="<?php echo htmlspecialchars($id)">
+            <input type="text" name="id" id="id" value="<?php echo htmlspecialchars($id); ?>">
           </fieldset>
           <fieldset class="row2">
             <legend>Ingredients</legend>
